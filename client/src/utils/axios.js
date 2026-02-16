a57be6629f90b4168cf44cfef7d3b8dab194d14f
Karan Sharma <karan03945@gmail.com>
2026-02-17 00:31:24 +0530

    localhost api

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://gigflow-n32p.onrender.com/api",
-  // baseURL: "http://localhost:5001/api",
+  // baseURL: "https://gigflow-n32p.onrender.com/api",
+  baseURL: "http://localhost:5001/api",
   withCredentials: true
 });
 
